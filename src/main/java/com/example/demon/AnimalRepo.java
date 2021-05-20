e1e2220f0839d00f5c93994bf5538e09f8c7d8fd
YehorUd <fifaha2017@gmail.com>
2021-05-20 16:49:42 +0200

    update 1.0

--- src/main/java/com/example/demon/AnimalRepo.java
+++ src/main/java/com/example/demon/AnimalRepo.java
@@ -2,9 +2,11 @@ package com.example.demon;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+
 import java.util.List;
 @Repository
 public interface AnimalRepo extends JpaRepository <com.example.demon.Animal, Integer> {
-    List<com.example.demon.Animal> findAllByName(String search);
+    List<com.example.demon.Animal> findByName(String search);
 }
 
